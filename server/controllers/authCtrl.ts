6d8c01bbfd543b6e25cb2250911bd4ef83c187be
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-08-31 03:39:23 +0530

    userCtrl --> register

---
+++ server/controllers/authCtrl.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import Users from "../models/userModel";
+import bcrypt from "bcrypt";
+import jwt from "jwt";
+
+const authCtrl = {
+  register: async (req: Request, res: Response) => {
+    try {
+      const { name, account, password } = req.body;
+
+      const user = await Users.findOne({ account });
+
+      if (user)
+        return res
+          .status(400)
+          .json({ msg: "Email or phone number already exists" });
+
+      const passwordHash = await bcrypt.hash(password, 12);
+
+      const newUser = { name, account, password: passwordHash };
+
+      res.json({
+        status: "OK",
+        msg: "register successful",
+        data: newUser,
+      });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+};
