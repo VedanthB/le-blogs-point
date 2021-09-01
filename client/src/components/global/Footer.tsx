c13d07464e16992741a327a74b8d29e357451d4d
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-09-01 11:01:17 +0530

    header and footer

---
+++ client/src/components/global/Footer.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="text-center bg-light py-4">
+      <h6>
+        "Don't ask what the world needs, ask what makes you come alive. Because
+        what the world needs most, is more people who come alive." - Howard
+        Thurman
+      </h6>
+      <a
+        href="https://github.com/VedanthB"
+        target="_blank"
+        rel="noreferrer"
+        className="mb-2 d-block"
+      >
+        GitHub
+      </a>
+      <p> Made with 💖 &copy; 2021</p>
+    </div>
+  );
+};
+
+export default Footer;
