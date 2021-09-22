db379e426fbcf23365fec837c85df6985991d7c3
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-09-23 02:06:19 +0530

    facebook Login Button --> SocalLogin --> components  ✨

--- client/src/components/auth/SocialLogin.tsx
+++ client/src/components/auth/SocialLogin.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { GoogleLogin, GoogleLoginResponse } from 'react-google-login-lite';
-import { googleLogin } from '../../redux/actions/authAction';
+import {
+  FacebookLogin,
+  FacebookLoginAuthResponse,
+} from 'react-facebook-login-lite';
+import { facebookLogin, googleLogin } from '../../redux/actions/authAction';
 
 const SocialLogin = () => {
   const dispatch = useDispatch();
+
+  const onFBSuccess = (response: FacebookLoginAuthResponse) => {
+    const { accessToken, userID } = response.authResponse;
+    dispatch(facebookLogin(accessToken, userID));
+  };
 
   const onSuccess = (googleUser: GoogleLoginResponse) => {
     const id_token = googleUser.getAuthResponse().id_token;
@@ -12,13 +21,19 @@ const SocialLogin = () => {
   };
 
   return (
-    <div className="my-2">
-      <GoogleLogin
-        client_id="785240130468-bkbdl5lk4sbos6af5rcktcgk2tc930dm.apps.googleusercontent.com"
-        cookiepolicy="single_host_origin"
-        onSuccess={onSuccess}
-      />
-    </div>
+    <>
+      <div className="my-2">
+        <GoogleLogin
+          client_id="785240130468-bkbdl5lk4sbos6af5rcktcgk2tc930dm.apps.googleusercontent.com"
+          cookiepolicy="single_host_origin"
+          onSuccess={onSuccess}
+        />
+      </div>
+
+      <div className="my-2">
+        <FacebookLogin appId="289445412656288" onSuccess={onFBSuccess} />
+      </div>
+    </>
   );
 };
 
