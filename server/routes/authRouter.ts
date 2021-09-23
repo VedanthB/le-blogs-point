4c680d38e8604bbe1bb6204b754b7827f1583026
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-09-24 03:31:01 +0530

    login_sms and sms_verify routes (server). ✨

--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -16,4 +16,8 @@ router.get('/refresh_token', authCtrl.refreshToken);
 
 router.post('/google_login', authCtrl.googleLogin);
 
+router.post('/login_sms', authCtrl.loginSMS);
+
+router.post('/sms_verify', authCtrl.smsVerify);
+
 export default router;
