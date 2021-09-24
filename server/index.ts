aaabf776c8867129f0c252de497224715c780b9c
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-09-25 03:02:52 +0530

    userRouter 🐶 (server)

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,29 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
-import routes from "./routes/index";
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import routes from './routes/index';
 
 // Middleware
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cookieParser());
 
 // Routes
-app.use("/api", routes.authRouter);
+app.use('/api', routes.authRouter);
+app.use('/api', routes.userRouter);
 
 // Database
-import "./config/database";
+import './config/database';
 
 //server listening
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("server is running on port:", PORT);
+  console.log('server is running on port:', PORT);
 });
