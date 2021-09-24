f5785b09cdeb3440745ec96b0884dcbdb638b74b
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-09-25 02:39:49 +0530

    redux profieAction client 🌟 patchAPI util function

--- client/src/utils/FetchData.ts
+++ client/src/utils/FetchData.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const postAPI = async (url: string, post: object, token?: string) => {
   const res = await axios.post(`/api/${url}`, post, {
@@ -10,6 +10,14 @@ export const postAPI = async (url: string, post: object, token?: string) => {
 
 export const getAPI = async (url: string, token?: string) => {
   const res = await axios.get(`/api/${url}`, {
+    headers: { Authorization: token },
+  });
+
+  return res;
+};
+
+export const patchAPI = async (url: string, post: object, token?: string) => {
+  const res = await axios.patch(`/api/${url}`, post, {
     headers: { Authorization: token },
   });
 
