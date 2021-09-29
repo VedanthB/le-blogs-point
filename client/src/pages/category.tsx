786ac3f304806809e0a04d2224351bb4e91ed60e
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-09-29 22:40:25 +0530

    delete and update category event handle in the category page 🌽

--- client/src/pages/category.tsx
+++ client/src/pages/category.tsx
@@ -1,25 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { FormSubmit, RootStore } from '../utils/Typescript';
+import { FormSubmit, ICategory, RootStore } from '../utils/Typescript';
 
-import { createCategory } from '../redux/actions/categoryAction';
+import {
+  createCategory,
+  deleteCategory,
+  updateCategory,
+} from '../redux/actions/categoryAction';
 
 import NotFound from '../components/global/NotFound';
 
 const Category = () => {
   const [name, setName] = useState('');
+  const [edit, setEdit] = useState<ICategory | null>(null);
 
   const { auth, categories } = useSelector((state: RootStore) => state);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    if (edit) setName(edit.name);
+  }, [edit]);
+
   const handleSubmit = (e: FormSubmit) => {
     e.preventDefault();
     if (!auth.access_token || !name) return;
-
-    dispatch(createCategory(name, auth.access_token));
-
+    if (edit) {
+      if (edit.name === name) return;
+      const data = { ...edit, name };
+      dispatch(updateCategory(data, auth.access_token));
+    } else {
+      dispatch(createCategory(name, auth.access_token));
+    }
     setName('');
+    setEdit(null);
+  };
+
+  const handleDelete = (id: string) => {
+    if (!auth.access_token) return;
+    dispatch(deleteCategory(id, auth.access_token));
   };
 
   if (auth.user?.role !== 'admin') return <NotFound />;
@@ -28,7 +47,14 @@ const Category = () => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="category">Category</label>
 
-        <div className="d-flex">
+        <div className="d-flex align-items-center">
+          {edit && (
+            <i
+              className="fas fa-times me-2 text-danger"
+              style={{ cursor: 'pointer' }}
+              onClick={() => setEdit(null)}
+            />
+          )}
           <input
             type="text"
             name="category"
@@ -37,7 +63,7 @@ const Category = () => {
             onChange={(e) => setName(e.target.value)}
           />
 
-          <button type="submit">Create</button>
+          <button type="submit">{edit ? 'Update' : 'Create'}</button>
         </div>
       </form>
 
@@ -47,8 +73,14 @@ const Category = () => {
             <p className="m-0 text-capitalize">{category.name}</p>
 
             <div>
-              <i className="fas fa-edit mx-2" />
-              <i className="fas fa-trash-alt" />
+              <i
+                className="fas fa-edit mx-2"
+                onClick={() => setEdit(category)}
+              />
+              <i
+                className="fas fa-trash-alt"
+                onClick={() => handleDelete(category._id)}
+              />
             </div>
           </div>
         ))}
