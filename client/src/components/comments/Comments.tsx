57b987beb1b55f7a923840cb46d0907437fac49b
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-10-15 21:19:07 +0530

    reply comment types and reply comments in comment list and comments 🔥

--- client/src/components/comments/Comments.tsx
+++ client/src/components/comments/Comments.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { IComment } from '../../utils/Typescript';
 
 import AvatarComment from './AvatarComment';
+import AvatarReply from './AvatarReply';
 import CommentList from './CommentList';
 
 interface IProps {
@@ -10,6 +11,8 @@ interface IProps {
 }
 
 const Comments: React.FC<IProps> = ({ comment }) => {
+  const [showReply, setShowReply] = useState<IComment[]>([]);
+
   return (
     <div
       className="my-3 d-flex"
@@ -20,7 +23,29 @@ const Comments: React.FC<IProps> = ({ comment }) => {
     >
       <AvatarComment user={comment.user} />
 
-      <CommentList comment={comment} />
+      <CommentList
+        comment={comment}
+        showReply={showReply}
+        setShowReply={setShowReply}
+      >
+        {showReply.map((comment, index) => (
+          <div
+            key={index}
+            style={{
+              opacity: comment._id ? 1 : 0.5,
+              pointerEvents: comment._id ? 'initial' : 'none',
+            }}
+          >
+            <AvatarReply user={comment.user} reply_user={comment.reply_user} />
+
+            <CommentList
+              comment={comment}
+              showReply={showReply}
+              setShowReply={setShowReply}
+            />
+          </div>
+        ))}
+      </CommentList>
     </div>
   );
 };
