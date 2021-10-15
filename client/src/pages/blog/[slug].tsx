093ca940ee1c77de95a44654768a047e8293a6b1
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-10-15 22:35:25 +0530

    join socket session

--- client/src/pages/blog/[slug].tsx
+++ client/src/pages/blog/[slug].tsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { IParams, IBlog } from '../../utils/Typescript';
+import { IParams, IBlog, RootStore } from '../../utils/Typescript';
 import { getAPI } from '../../utils/FetchData';
 
 import Loading from '../../components/global/Loading';
 import { showErrMsg } from '../../components/alert/Alert';
 import DisplayBlog from '../../components/blog/DisplayBlog';
+import { useSelector } from 'react-redux';
 
 const DetailBlog = () => {
   const id = useParams<IParams>().slug;
+
+  const { socket } = useSelector((state: RootStore) => state);
 
   const [blog, setBlog] = useState<IBlog>();
   const [loading, setLoading] = useState(false);
@@ -32,6 +35,16 @@ const DetailBlog = () => {
 
     return () => setBlog(undefined);
   }, [id]);
+
+  // Join Room
+  useEffect(() => {
+    if (!id || !socket) return;
+    socket.emit('joinRoom', id);
+
+    return () => {
+      socket.emit('outRoom', id);
+    };
+  }, [socket, id]);
 
   if (loading) return <Loading />;
   return (
