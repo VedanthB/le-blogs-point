fe097bbe3912fd3f584cd4dfaa1b90a3c9cc8450
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-10-15 21:39:13 +0530

    pagination in get comments and reply comment ctrl function

--- server/controllers/commentCtrl.ts
+++ server/controllers/commentCtrl.ts
@@ -44,13 +44,18 @@ const commentCtrl = {
               {
                 $match: {
                   blog_id: new mongoose.Types.ObjectId(req.params.id),
+                  comment_root: { $exists: false },
+                  reply_user: { $exists: false },
                 },
               },
               {
                 $lookup: {
                   from: 'users',
-                  localField: 'user',
-                  foreignField: '_id',
+                  let: { user_id: '$user' },
+                  pipeline: [
+                    { $match: { $expr: { $eq: ['$_id', '$$user_id'] } } },
+                    { $project: { name: 1, avatar: 1 } },
+                  ],
                   as: 'user',
                 },
               },
@@ -64,8 +69,11 @@ const commentCtrl = {
                     {
                       $lookup: {
                         from: 'users',
-                        localField: 'user',
-                        foreignField: '_id',
+                        let: { user_id: '$user' },
+                        pipeline: [
+                          { $match: { $expr: { $eq: ['$_id', '$$user_id'] } } },
+                          { $project: { name: 1, avatar: 1 } },
+                        ],
                         as: 'user',
                       },
                     },
@@ -73,8 +81,11 @@ const commentCtrl = {
                     {
                       $lookup: {
                         from: 'users',
-                        localField: 'reply_user',
-                        foreignField: '_id',
+                        let: { user_id: '$reply_user' },
+                        pipeline: [
+                          { $match: { $expr: { $eq: ['$_id', '$$user_id'] } } },
+                          { $project: { name: 1, avatar: 1 } },
+                        ],
                         as: 'reply_user',
                       },
                     },
