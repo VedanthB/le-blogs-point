80ee13b7ba0c4f0813eefe1dc9cbae6974ec3470
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-10-15 21:34:59 +0530

    reply comment fix 😆

--- client/src/components/comments/CommentList.tsx
+++ client/src/components/comments/CommentList.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { replyComment } from '../../redux/actions/commentAction';
 
 import { IComment, RootStore } from '../../utils/Typescript';
 
