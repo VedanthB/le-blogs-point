7962799c5dff6f2f548d615c8986b05004e062b5
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-10-16 20:00:41 +0530

    update blog action function 🦑

--- client/src/redux/actions/blogAction.ts
+++ client/src/redux/actions/blogAction.ts
@@ -2,7 +2,7 @@ import { Dispatch } from 'redux';
 import { IBlog } from '../../utils/Typescript';
 import { imageUpload } from '../../utils/ImageUpload';
 import { ALERT, IAlertType } from '../types/alertType';
-import { getAPI, postAPI } from '../../utils/FetchData';
+import { getAPI, postAPI, putAPI } from '../../utils/FetchData';
 import {
   GET_BLOGS_CATEGORY_ID,
   GET_BLOGS_USER_ID,
@@ -93,6 +93,29 @@ export const getBlogsByUserId =
       });
 
       dispatch({ type: ALERT, payload: { loading: false } });
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+    }
+  };
+
+export const updateBlog =
+  (blog: IBlog, token: string) => async (dispatch: Dispatch<IAlertType>) => {
+    let url;
+    try {
+      dispatch({ type: ALERT, payload: { loading: true } });
+
+      if (typeof blog.thumbnail !== 'string') {
+        const photo = await imageUpload(blog.thumbnail);
+        url = photo.url;
+      } else {
+        url = blog.thumbnail;
+      }
+
+      const newBlog = { ...blog, thumbnail: url };
+
+      const res = await putAPI(`blog/${newBlog._id}`, newBlog, token);
+
+      dispatch({ type: ALERT, payload: { success: res.data.msg } });
     } catch (err: any) {
       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
     }
