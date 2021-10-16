436bf6d4fef60c15330bbb16a80ca9568dfb6a25
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-10-16 21:45:25 +0530

    added auth middleware to logout route 🐥

--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import authCtrl from '../controllers/authCtrl';
+import auth from '../middleware/auth';
 import { validRegister } from '../middleware/valid';
 
 const router = express.Router();
@@ -10,7 +11,7 @@ router.post('/active', authCtrl.activeAccount);
 
 router.post('/login', authCtrl.login);
 
-router.get('/logout', authCtrl.logout);
+router.get('/logout', auth, authCtrl.logout);
 
 router.get('/refresh_token', authCtrl.refreshToken);
 
